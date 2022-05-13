82da47893c8e1d68a1a094edd5b61dbecfdbd3f6
Dineshraj8 <dineshraj2809@gmail.com>
2022-05-13 11:42:07 +0000

    Changed IP Address

--- emotion-frontend/src/api/backend.tsx
+++ emotion-frontend/src/api/backend.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const GetImageData = async () =>{
-    const response = await axios.get("http://localhost:3001/image-id");
+    const response = await axios.get("http://43.204.11.138:3001/image-id");
     return response.data.data[0].Data[0];
 }
 export const GetTextData = async () =>{
-    const response = await axios.get("http://localhost:3001/text-id");
+    const response = await axios.get("http://43.204.11.138:3001/text-id");
     return response.data.data;
 }

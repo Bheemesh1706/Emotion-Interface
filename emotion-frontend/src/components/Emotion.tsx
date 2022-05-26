450bfce1b594227760219bcbb8e474de1b2a1799
dinesh <dineshraj2809@gmail.com>
2022-05-26 21:12:24 +0530

    For diff labels added diff colors

--- emotion-frontend/src/components/Emotion.tsx
+++ emotion-frontend/src/components/Emotion.tsx
@@ -51,7 +51,25 @@ function Emotion() {
           {
             label: 'Emotions',
             data: [graphdata?.Angry*10,graphdata?.Disgust*10,graphdata?.Fear*10,graphdata?.Happy*10,graphdata?.Sad*10,graphdata?.Surprise*10,graphdata?.Neutral*10],
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.5)',
+              'rgba(255, 159, 64, 0.5)',
+              'rgba(255, 205, 86, 0.2)',
+              'rgba(75, 192, 192, 0.5)',
+              'rgba(54, 162, 235, 0.5)',
+              'rgba(153, 102, 255, 0.5)', 
+              'rgba(201, 203, 207, 0.5)'
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 159, 64)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
           }
         ],
       };

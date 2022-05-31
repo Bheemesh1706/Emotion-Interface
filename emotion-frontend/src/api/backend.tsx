dd40773ba942065f7df3e93fe0e709ffa692e832
dinesh <dineshraj2809@gmail.com>
2022-05-31 19:37:01 +0530

    worked on sentiment bar chart

--- emotion-frontend/src/api/backend.tsx
+++ emotion-frontend/src/api/backend.tsx
@@ -2,10 +2,12 @@ import axios from "axios";
 
 export const GetImageData = async () =>{
     const response = await axios.get("http://43.204.11.138:3001/image-id")
+    console.log (response.data.data[0]);
     return response.data.data[0];
 }
 export const GetTextData = async () =>{
     const response = await axios.get("http://43.204.11.138:3001/text-id");
+    console.log (response.data.data);
     return response.data.data;
 }
 
@@ -18,7 +20,13 @@ export const  Scrapetext =async () => {
 }
 
 export const GetAllImageData = async () =>{
-    const response = await axios.get("http://localhost:3500/imagedata");
+    const response = await axios.get("http://43.204.11.138:3500/imagedata");
+    console.log(response.data.data);
+    return response.data.data;
+}
+
+export const GetSentimentData = async () =>{
+    const response = await axios.get("http://43.204.11.138:3001/Sentiment-id")
     console.log(response.data.data);
     return response.data.data;
 }
--- emotion-frontend/src/components/Sentiment.tsx
+++ emotion-frontend/src/components/Sentiment.tsx
@@ -1,6 +1,6 @@
-import {Bar} from 'react-chartjs-2';
-import { useEffect, useState } from 'react';
-import { GetImageData } from '../api/backend';
+import { Bar } from "react-chartjs-2";
+import { useEffect, useState } from "react";
+import { GetSentimentData } from "../api/backend";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,7 +9,7 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from "chart.js";
 
 ChartJS.register(
   CategoryScale,
@@ -20,63 +20,55 @@ ChartJS.register(
   Legend
 );
 
+function Sentiment() {
+  const [sentidata, setSentiData] = useState<any>();
 
-function Emotion() {
+  useEffect(() => {
+    GetSentimentData().then((e) => {
+      setSentiData(e);
+    });
+  });
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Bar Chart",
+      },
+    },
+  };
 
-    const [graphdata,setGraphData] = useState<any>();
+  const labels = ["Happy", "Neutal", "Sad"];
 
-    useEffect(()=>{
-        GetImageData().then((e)=>{
-          setGraphData(e)
-        });
-    })
-    const options = {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top' as const,
-          },
-          title: {
-            display: true,
-            text: 'Bar Chart',
-          },
-        },
-      };
-      
-      const labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise' ,'Neutal'];
-      
-      const data = {
-        labels,
-        datasets: [
-          {
-            label: 'Emotions',
-            data: [1,2,3,4,5,6,7],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.5)',
-              'rgba(255, 159, 64, 0.5)',
-              'rgba(255, 205, 86, 0.2)',
-              'rgba(75, 192, 192, 0.5)',
-              'rgba(54, 162, 235, 0.5)',
-              'rgba(153, 102, 255, 0.5)', 
-              'rgba(201, 203, 207, 0.5)'
-            ],
-            borderColor: [
-              'rgb(255, 99, 132)',
-              'rgb(255, 159, 64)',
-              'rgb(255, 205, 86)',
-              'rgb(75, 192, 192)',
-              'rgb(54, 162, 235)',
-              'rgb(153, 102, 255)',
-              'rgb(201, 203, 207)'
-            ],
-            borderWidth: 1
-          }
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Sentiments",
+        data: [
+          sentidata?.Happy * 10000000000000000,
+          sentidata?.Neutral * 10000000000000000,
+          sentidata?.Sad * 10000000000000000,
         ],
-      };
-      
-    
-      return <Bar options={options} data={data} />;
-     
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+        ],
+        borderColor: [
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Bar options={options} data={data} />;
 }
 
-export default Emotion
+export default Sentiment;
